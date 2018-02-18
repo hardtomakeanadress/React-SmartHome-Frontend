843e5169a7f5902bd79c56c3bbb19f84e685cc8a
adi-laptop <hardtomakeanadress@hotmail.com>
2018-02-18 13:26:28 +0200

    change css for sensor text align

--- src/components/Sensor/Sensor.js
+++ src/components/Sensor/Sensor.js
@@ -3,9 +3,8 @@ import './Sensor.css';
 
 const Sensor = (props) => {
     return (
-        <div className='Sensor'>
+        <div className='sensor'>
             <div className="container">
-              <h5>Sensors:</h5>
               <div>Temperature: {props.temperature} &#8451;</div>
               <div>Humidity: {props.humidity} %</div>
               <div>Voltage: {props.voltage} V</div>
